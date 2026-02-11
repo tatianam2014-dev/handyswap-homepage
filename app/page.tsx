5f324634c0c26ce5700652220477adb96d3fd145
tatianam2014-dev <tatiana.m.2014@gmail.com>
2026-02-11 09:39:10 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "./components/Hero";
+import USPStrip from "./components/USPStrip";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <USPStrip />
     </>
   );
 }
